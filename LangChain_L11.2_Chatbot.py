d565a0e886e4796a6a7f55f3e65b52f4a34310f4
Shantnu-singh <singhshantnu2001@gmail.com>
2025-08-23 22:16:14 +0530

    Added ChatBots in LangChain

--- LangChain_L11.2_Chatbot.py
+++ LangChain_L11.2_Chatbot.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from LangChain_L11_Chatbot import chatbot , HumanMessage
 CONFIG = {'configurable': {'thread_id': 'thread-1'}}
-
+# st.set_page_config(layout="wide")
 
 # Create a session state
 message_history = []
@@ -11,19 +11,8 @@ if 'msg_hist' not in st.session_state:
 
 for msg in st.session_state['msg_hist']:
     with st.chat_message(msg['role']):
-        st.text(msg['content'])
+        st.markdown(msg['content'])
 
-# with st.chat_message("user"):
-#     st.text("HI")
-    
-# with st.chat_message("assistant"):
-#     st.text("Hey to you too..")
-    
-# with st.chat_message("user"):
-#     st.text("My name is shantnu")
-
-# with st.chat_message("assistant"):
-#     st.text("Hey Shantnu, How can i help you.")
     
 user_input = st.chat_input("Type here...")
 
@@ -31,10 +20,10 @@ if user_input:
     # First add user msg to hist
     st.session_state['msg_hist'].append({'role' : 'user' , 'content' : user_input })
     with st.chat_message("user"):
-        st.text(user_input)
+        st.markdown(user_input)
     
     responce = chatbot.invoke({"messages" : [HumanMessage(content=user_input)]} , config= CONFIG)
     # Save assistance msg in hist
     st.session_state['msg_hist'].append({'role' : 'assistant' , 'content' : responce['messages'][-1].content})
     with st.chat_message("assistant"):
-        st.text(responce['messages'][-1].content)
+        st.markdown(responce['messages'][-1].content)
